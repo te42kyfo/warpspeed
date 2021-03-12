af818e09fb6370f928e48f4628faa0e63780e699
Dominik Ernst <dominik.ernst@fau.de>
2021-03-12 17:10:09 +0100

    ISL: account for two previous waves, printing changes

--- volumes_isl.py
+++ volumes_isl.py
@@ -43,10 +43,20 @@ def getMemLoadBlockVolumeISL(
         (metaGridId - 1) // (metaGrid[0] * metaGrid[1]),
     )
 
-    print(metaGrid)
-    print(metaGridId)
-    print(metaGridPosition)
-    print(prevMetaGridPosition)
+    prev2MetaGridPosition = (
+        (metaGridId - 2) % metaGrid[0],
+        (metaGridId - 2) // metaGrid[0] % metaGrid[1],
+        (metaGridId - 2) // (metaGrid[0] * metaGrid[1]),
+    )
+
+    if (
+        prev2MetaGridPosition[0] == prevMetaGridPosition[0]
+        or prev2MetaGridPosition[1] == prevMetaGridPosition[1]
+    ):
+        prev2MetaGridPosition = prevMetaGridPosition
+
+    print("ISL Meta Grid:" + str(metaGrid))
+
 
     concurrentThreads = tuple([block[i] * concurrentGrid[i] for i in range(3)])
 
@@ -60,9 +70,10 @@ def getMemLoadBlockVolumeISL(
     # print(previousDomain)
 
     cellCount = 0
-    Vcomplete = 0
+    Vold = 0
     Vnew = 0
     Voverlap = 0
+
     for field in loadExprs:
         accesses = None
         for access in loadExprs[field]:
@@ -99,11 +110,23 @@ def getMemLoadBlockVolumeISL(
                 prevMetaGridPosition[2] * concurrentThreads[2] + tidz + 1,
             )
 
+            prev2TidzSliceString = " {{[tidx, tidy, tidz] : {0} <= tidx < {3} and {1} <= tidy < {4} and {2} <= tidz < {5}}}".format(
+                *(prev2MetaGridPosition[i] * concurrentThreads[i] for i in range(2)),
+                prev2MetaGridPosition[2] * concurrentThreads[2] + tidz,
+                *(
+                    (prev2MetaGridPosition[i] + 1) * concurrentThreads[i]
+                    for i in range(2)
+                ),
+                prev2MetaGridPosition[2] * concurrentThreads[2] + tidz + 1,
+            )
             tidzSliceDomain = isl.BasicSet(tidzSliceString).intersect(validDomainSet)
             prevTidzSliceDomain = isl.BasicSet(prevTidzSliceString).intersect(
                 validDomainSet
             )
 
+            prev2TidzSliceDomain = isl.BasicSet(prev2TidzSliceString).intersect(
+                validDomainSet
+            )
             if not addresses is None:
                 addresses = addresses.union(tidzSliceDomain.apply(accesses))
             else:
@@ -111,18 +134,24 @@ def getMemLoadBlockVolumeISL(
 
             if not prevAddresses is None:
                 prevAddresses = prevAddresses.union(prevTidzSliceDomain.apply(accesses))
+                prevAddresses = prevAddresses.union(
+                    prev2TidzSliceDomain.apply(accesses)
+                )
             else:
                 prevAddresses = prevTidzSliceDomain.apply(accesses)
+                prevAddresses = prevAddresses.union(
+                    prev2TidzSliceDomain.apply(accesses)
+                )
             cellCount += tidzSliceDomain.count_val().to_python()
 
-        Vcomplete += addresses.count_val().to_python()
-        Vnew += addresses.subtract(prevAddresses).count_val().to_python()
+        Vnew += addresses.count_val().to_python()
+        Vold += prevAddresses.count_val().to_python()
         Voverlap += addresses.intersect(prevAddresses).count_val().to_python()
 
     cellCount = max(1, cellCount)  # avoid division by zero
     return (
-        Vcomplete * 32 / cellCount * (block[0] * block[1] * block[2]),
         Vnew * 32 / cellCount * (block[0] * block[1] * block[2]),
+        Vold * 32 / cellCount * (block[0] * block[1] * block[2]),
         Voverlap * 32 / cellCount * (block[0] * block[1] * block[2]),
         cellCount,
     )
