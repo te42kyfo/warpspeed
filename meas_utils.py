b571347718af54ca1f81e37de0dac050c984e9e8
Dominik Ernst <dominik.ernst@fau.de>
2022-03-02 10:12:56 +0100

    meas_utils: specify metrics to be measured in python instead of compiled into measureMEtrics

--- meas_utils.py
+++ meas_utils.py
@@ -27,7 +27,7 @@ def timeKernel(run_func):
 
 def benchKernel(run_func):
     run_func()
-    repeats = [timeKernel(run_func) for i in range(0, 5)]
+    repeats = [timeKernel(run_func) for i in range(0, 7)]
     repeats.sort()
     time = repeats[len(repeats) // 2]
     return time
@@ -38,20 +38,42 @@ def measureMetrics(runFunc, size):
 
     measurements = []
     for i in range(0, 7):
-        measureMetric.measureBandwidthStart()
+        measureMetric.measureMetricStart(
+            [
+                "dram__bytes_read.sum",
+                "dram__bytes_write.sum",
+                "lts__t_sectors_op_read.sum",
+                "lts__t_sectors_op_write.sum",
+                "lts__t_sectors_srcunit_tex_op_read.sum",
+                "lts__t_sectors_srcunit_tex_op_write.sum",
+                "lts__t_sectors_srcunit_tex.sum",
+                #"lts__t_sectors_srcunit_ltcfabric.sum",
+                #"lts__t_sectors.sum",
+                "lts__t_tag_requests.sum",
+                "l1tex__data_pipe_lsu_wavefronts.sum",
+            ]
+        )
         runFunc()
         measurements.append(measureMetric.measureMetricStop())
 
     medians = [
-        sorted([m[i] for m in measurements])[len(measurements) // 2] for i in range(len(measurements[0]))
+        sorted([m[i] for m in measurements])[len(measurements) // 2]
+        for i in range(len(measurements[0]))
     ]
 
-
-    return  medians[0] / lupCount, medians[1] / lupCount, medians[2] * 32 / lupCount,  medians[3] * 32 / lupCount, medians[4]*32 / lupCount, medians[5]*32 / lupCount, medians[6]*32 / lupCount, medians[7]*32 / lupCount, medians[8]*32 / lupCount, medians[9] / lupCount
-
-
-
-
+    return (
+        medians[0] / lupCount,
+        medians[1] / lupCount,
+        medians[2] * 32 / lupCount,
+        medians[3] * 32 / lupCount,
+        medians[4] * 32 / lupCount,
+        medians[5] * 32 / lupCount,
+        medians[6] * 32 / lupCount,
+        #medians[7] * 32 / lupCount,
+        #medians[8] * 32 / lupCount,
+        medians[7] / lupCount,
+        medians[8] / lupCount,
+    )
 
 
 def printSASS(code):
--- measured_metrics.py
+++ measured_metrics.py
@@ -2,10 +2,26 @@
 from meas_utils import measureMetrics, benchKernel
 from column_print import *
 
+
 class MeasuredMetrics:
-    def measure( runFunc, lc):
+    def measure(runFunc, lc):
         self = MeasuredMetrics()
-        self.memLoad, self.memStore, self.L2Load, self.L2Store, self.L2Load_tex, self.L2Store_tex, self.L2tex, self.L2ltc, self.L2total, self.L2tagRequests = measureMetrics(runFunc, lc.domain)
+        (
+            self.memLoad,
+            self.memStore,
+            self.L2Load,
+            self.L2Store,
+            self.L2Load_tex,
+            self.L2Store_tex,
+            self.L2tex,
+            #self.L2ltc,
+            #self.L2total,
+            self.L2tagRequests,
+            self.L1Wavefronts,
+        ) = measureMetrics(runFunc, lc.domain)
+
+        self.L2ltc = 1
+        self.L2total = 1
 
         time = benchKernel(runFunc)
         self.lups = lc.domain[0] * lc.domain[1] * lc.domain[2] / time / 1e6
@@ -19,13 +35,27 @@ class MeasuredMetrics:
     def stringKey(self, key, labelWidth, valueWidth):
         kB = False
         if key in self.__dict__:
-            string = "{:{labelWidth}}: {:{valueWidth}.1f}{:2} ".format(str(key),  self.__dict__[key] / (1024 if kB else 1), "kB" if kB else "",
-                                                                         labelWidth=labelWidth, valueWidth=valueWidth)
+            string = "{:{labelWidth}}: {:{valueWidth}.1f}{:2} ".format(
+                str(key),
+                self.__dict__[key] / (1024 if kB else 1),
+                "kB" if kB else "",
+                labelWidth=labelWidth,
+                valueWidth=valueWidth,
+            )
         else:
-            string = "{:{width}}".format(" ", width=labelWidth+valueWidth)
+            string = "{:{width}}".format(" ", width=labelWidth + valueWidth)
         return string
+
     def __str__(self):
-        columns = [["L2Load"], ["L2Store"],["memLoad"],["memStore"],["lups"], ["L1Cycles"], ["L1Volume"]]
+        columns = [
+            ["L2Load"],
+            ["L2Store"],
+            ["memLoad"],
+            ["memStore"],
+            ["lups"],
+            ["L1Wavefronts"],
+            ["L1Volume"],
+        ]
 
         return columnPrint(self, columns)
 
@@ -35,15 +65,12 @@ class ResultComparer:
         self.meas = meas
         self.pred = pred
 
-
     def stringKey(self, key, labelWidth, valueWidth):
-
 
         if key.startswith("meas"):
             return self.meas.stringKey(key[4:], labelWidth, valueWidth)
         else:
             return self.pred.stringKey(key, labelWidth, valueWidth)
-
 
         #     value = self.meas.__dict__[key[4:]]if key[4:] in self.meas.__dict__ else None
         # else:
@@ -54,12 +81,29 @@ class ResultComparer:
         # else:
         #     return "{:{width}}".format(" ", width=labelWidth+valueWidth+8)
 
-
     def __str__(self):
-        columns = [["L1Cycles", "L1Load", "smL1Alloc", "L1LoadEvicts", "L2LoadV1", "L2LoadV2", "measL2Load" ],
-                   ["memLoadOverlap", "memLoadEvicts", "memLoadV1", "memLoadV2", "memLoadV3", "memLoadV4", "measmemLoad"],
-                   ["waveL2Alloc", "L2NewCoverage",  "L2Store", "measL2Store"],
-                   ["memStoreEvicts", "memStoreV1", "memStoreV2", "measmemStore"],
-                   ["perfPheno", "perfV1", "perfV2", "perfV3", "perfV4", "measlups"]]
+        columns = [
+            [
+                "L1Cycles",
+                "L1Load",
+                "smL1Alloc",
+                "L1LoadEvicts",
+                "L2LoadV1",
+                "L2LoadV2",
+                "measL2Load",
+            ],
+            [
+                "memLoadOverlap",
+                "memLoadEvicts",
+                "memLoadV1",
+                "memLoadV2",
+                "memLoadV3",
+                "memLoadV4",
+                "measmemLoad",
+            ],
+            ["waveL2Alloc", "L2NewCoverage", "L2Store", "measL2Store"],
+            ["memStoreEvicts", "memStoreV1", "memStoreV2", "measmemStore"],
+            ["perfPheno", "perfV1", "perfV2", "perfV3", "perfV4", "measlups"],
+        ]
 
         return columnPrint(self, columns)
