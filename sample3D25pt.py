86a74a431a0c073a202c2433aa576e97b94d53c7
Dominik Ernst <dominik.ernst@gmx.de>
2022-04-27 15:17:38 +0200

    more details in README, 3D25pt sample

---
+++ sample3D25pt.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from predict_metrics import *
+from warpspeedkernel import *
+
+
+blockSize = (256, 2, 2)
+domain = (640, 512, 512)
+blockingFactors = (1,1,1)
+device = DeviceAmpere()
+
+loads = [("tidx", "tidy", "tidz")]
+
+for i in range(4):
+    loads.extend([("tidx+" + str(i), "tidy", "tidz"),
+                  ("tidx-" + str(i), "tidy", "tidz"),
+                  ("tidx", "tidy+" + str(i), "tidz"),
+                  ("tidx", "tidy-" + str(i), "tidz"),
+                  ("tidx", "tidy", "tidz+" + str(i)),
+                  ("tidx", "tidy", "tidz-" + str(i)),
+                  ])
+
+kernel =  WarpspeedGridKernel({"A" : loads},
+                              {"B" : [("tidx", "tidy", "tidz")]},
+                              domain, 32, 1)
+
+lc = LaunchConfig.compute(kernel, blockSize, domain, blockingFactors, device)
+basic = BasicMetrics.compute(lc, device, kernel)
+pred = DerivedMetrics(lc, basic, device)
+
+
+print(basic)
+print(pred)
