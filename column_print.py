bb45d3cc378c406a7682a3e572a2763585461840
Dominik Ernst <dominik.ernst@gmx.de>
2022-04-27 14:49:36 +0200

    unify stringKey function for column pretty printer, include units

--- column_print.py
+++ column_print.py
@@ -1,23 +1,47 @@
 #!/usr/bin/env python3
 
 
+def stringKey(self, key, labelWidth, valueWidth, siunit):
+
+    try:
+        value = eval("self." + key)
+    except:
+        value = ""
+
+    if value == "":
+        return "{:{width}}".format(" ", width=labelWidth + valueWidth + 1)
+
+
+    if siunit == "kB":
+        value /= 1024
+
+    if siunit == "MB":
+        value /= 1024*1024
+
+    prec = 0 if isinstance(value, int) else 1 if value < 50 else 0
+
+    return "{:{labelWidth}}: {:{valueWidth}.{prec}f} {:7}".format(
+            str(key),
+            value,
+            siunit,
+            labelWidth=labelWidth,
+            valueWidth=valueWidth,
+            prec=prec)
+
 def columnPrint(obj, columns):
 
     string = ""
-    columnLabelWidths = [max([len(e) for e in c]) for c in columns]
-    columnValueWidths = [max([ len((str(  int(obj.__dict__[e]) if isinstance( obj.__dict__[e], float) else obj.__dict__[e] ))) if e in obj.__dict__ else 5 for e in c]) for c in columns]
-
-
-
+    columnLabelWidths = [max([len(e[0]) for e in c]) for c in columns]
+    columnValueWidths = [max([len(stringKey(obj, e[0], 0, 0, e[1])) - len(e[0]) for e in c]) for c in columns]
 
     rowCount = max([len(c) for c in columns])
     for row in range(rowCount):
         rowx = rowCount - row
         for col in range(len(columns)):
             if rowx <= len(columns[col]):
-                string += obj.stringKey(columns[col][len(columns[col])-rowx], columnLabelWidths[col], columnValueWidths[col])
+                string += stringKey(obj, columns[col][len(columns[col])-rowx][0], columnLabelWidths[col], columnValueWidths[col] - 9, columns[col][len(columns[col])-rowx][1])
             else:
-                string += obj.stringKey("", columnLabelWidths[col], columnValueWidths[col])
+                string += stringKey(obj, "", columnLabelWidths[col], columnValueWidths[col], "")
         string += "\n"
     return string
 
