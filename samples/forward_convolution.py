e5ee07da93fe81cdaf1c2d367c78c2f7daf622f7
Dominik Ernst <dominik.ernst@gmx.de>
2022-12-22 17:19:44 +0100

    Add forward convolution sample, extend miniFE with SpMVM

---
+++ samples/forward_convolution.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+import sys
+sys.path.append('..')
+
+from predict_metrics import *
+import sympy as sp
+from warpspeedkernel import *
+
+imageSize = (1024, 1024)
+blockSize = (512, 1, 1)
+
+device = DeviceAmpere()
+
+for imageCount in [1, 8]:
+    for inputChannels in [1, 4, 8]:
+        for outputChannels in [1,4,8]:
+            print(str(inputChannels) + " x " + str(outputChannels))
+
+            loadFields = []
+            storeFields = []
+
+            imageLoads = []
+
+            for imageChannel in range(inputChannels):
+                for x in [-1,0,1]:
+                    for y in [-1, 0, 1]:
+                        imageLoads.append(("tidx + " + str(x), "tidy + " + str(y), str(imageChannel) ))
+
+            stencilLoads = []
+            for x in range(3):
+                for y in range(3):
+                    for outputChannel in range(outputChannels):
+                        stencilLoads.append((  str(x), str(y), str(outputChannel) ))
+
+            loadFields.append(Field(name = "stencil",
+                                    addresses = stencilLoads,
+                                    datatype = 4,
+                                    dimensions = (3,3,outputChannels),
+                                    alignment = 0))
+
+            for image in range(imageCount):
+                loadFields.append(Field(name = "images",
+                                    addresses = imageLoads,
+                                    datatype = 4,
+                                    dimensions = (*imageSize, inputChannels),
+                                    alignment = 0 ))
+
+
+            outputStores = []
+            for outputChannel in range(outputChannels):
+                outputStores.append(("tidx", "tidy", str(outputChannel) ))
+
+            for image in range(imageCount):
+                storeFields.append(Field(name = "output",
+                                        addresses = outputStores,
+                                        datatype = 4,
+                                        dimensions = (*imageSize, outputChannels),
+                                        alignment = 0))
+
+
+
+            kernel = WarpspeedKernel(loadFields, storeFields, registers = 128, flops = imageCount*inputChannels*outputChannels*2)
+
+            lc = LaunchConfig.compute(kernel, blockSize, (imageSize[0]-2, imageSize[1]-2, 1), (1,1,1), device)
+            basic = BasicMetrics.compute(lc, device, kernel)
+            pred = DerivedMetrics(lc, basic, device)
+
+            print(pred)
--- samples/miniFE.py
+++ samples/miniFE.py
@@ -7,7 +7,6 @@ from predict_metrics import *
 import sympy as sp
 from warpspeedkernel import *
 
-import matplotlib.pyplot as plt
 
 
 blockSize = (512, 1, 1)
@@ -18,60 +17,58 @@ blockingFactors = (1, 1, 1)
 device = DeviceAmpere()
 
 
-xySizes = [1, 2, 4, 8, 16] + [i * 32 for i in range(1, 16)] + [1024]
+xySizes = [64, 256, 1024]
 preds = []
 
-for xy in xySizes:
-    domain = (xy, xy, 512 * 256 * 256 // (xy * xy))
 
-    loadFields = []
-    storeFields = []
 
-    xloads = []
-    for z in {-1, 0, 1}:
-        for y in {-1, 0, 1}:
-            for x in {-1, 0, 1}:
-                xloads.append(
-                    ("tidx + " + str(x), "tidy + " + str(y), "tidz + " + str(z))
+for vectorCount in [1, 4, 16]:
+    for xy in [64, 256, 1024]:
+        domain = (xy, xy, 512 * 256 * 256 // (xy * xy))
+
+        print("Domain: " + str(domain) + ", " + str(vectorCount) + " vectors")
+
+        loadFields = []
+        storeFields = []
+
+        xloads = []
+        for z in {-1, 0, 1}:
+            for y in {-1, 0, 1}:
+                for x in {-1, 0, 1}:
+                    xloads.append(
+                        ("tidx + " + str(x), "tidy + " + str(y), "tidz + " + str(z))
+                    )
+        for i in range(vectorCount):
+            loadFields.append(Field("X" + str(i), xloads, 8, [d + 2 for d in domain], 0))
+
+        matrixLoads = []
+        for row in range(0, 27):
+            matrixLoads.append(
+                (
+                    "(tidx + tidy * {0} + tidz * {0} * {1}) + {3} * {0} * {1} * {2}".format(
+                        domain[0], domain[1], domain[2], row
+                    ),
+                    "0",
+                    "0",
                 )
-    loadFields.append(Field("X", xloads, 8, [d + 2 for d in domain], 0))
-
-    matrixLoads = []
-    for row in range(0, 27):
-        matrixLoads.append(
-            (
-                "(tidx + tidy * {0} + tidz * {0} * {1}) + {3} * {0} * {1} * {2}".format(
-                    domain[0], domain[1], domain[2], row
-                ),
-                "0",
-                "0",
             )
+
+        loadFields.append(
+            Field("mat", matrixLoads, 8, (domain[0], domain[1], domain[2] * 27), 0)
+        )
+        loadFields.append(
+            Field("idx", matrixLoads, 4, (domain[0], domain[1], domain[2] * 27), 0)
+        )
+        storeFields.append(
+            Field("Y", [("tidx", "tidy", "tidz")], 8, [d + 2 for d in domain], 0)
         )
 
-    loadFields.append(
-        Field("mat", matrixLoads, 8, (domain[0], domain[1], domain[2] * 27), 0)
-    )
-    loadFields.append(
-        Field("idx", matrixLoads, 4, (domain[0], domain[1], domain[2] * 27), 0)
-    )
-    storeFields.append(
-        Field("Y", [("tidx", "tidy", "tidz")], 8, [d + 2 for d in domain], 0)
-    )
+        kernel = WarpspeedKernel(loadFields, storeFields, 64, flops=27*2*vectorCount)
 
-    kernel = WarpspeedKernel(loadFields, storeFields, 64, flops=27 * 2)
+        lc = LaunchConfig.compute(kernel, blockSize, domain, blockingFactors, device)
+        basic = BasicMetrics.compute(lc, device, kernel)
+        pred = DerivedMetrics(lc, basic, device)
 
-    lc = LaunchConfig.compute(kernel, blockSize, domain, blockingFactors, device)
-    basic = BasicMetrics.compute(lc, device, kernel)
-    pred = DerivedMetrics(lc, basic, device)
+        preds.append(pred)
 
-    preds.append(pred)
-
-    print(basic)
-    print(pred)
-
-
-fig, ax = plt.subplots(figsize=(6, 3.5))
-ax.plot(xySizes, [pred.perfV3 for pred in preds])
-ax.set_ylim((0, ax.get_ylim()[1]))
-
-plt.show()
+        print(pred)
