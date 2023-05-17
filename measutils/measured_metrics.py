1c8e177310f2755981d7d7c44bcaef0efa152a76
Dominik Ernst <dominik.ernst@gmx.de>
2023-05-17 19:08:59 +0200

    measured_metrics: meaure both rocm and cuda

--- measutils/measured_metrics.py
+++ measutils/measured_metrics.py
@@ -6,41 +6,131 @@ from pystencils.display_utils import get_code_str
 
 class MeasuredMetrics:
     def measure(kernel, lc):
-
         codeText = (
             get_code_str(kernel)
             .replace("FUNC_PREFIX", '#define int64_t size_t \n extern "C" __global__ ')
             .replace("RESTRICT", "__restrict__")
+            # .replace("blockIdx.x", "0")
+            # .replace("blockIdx.y", "0")
+            # .replace("blockIdx.z", "0")
+            # .replace("threadIdx.x", "(threadIdx.x%64)")
+            # .replace("threadIdx.y", "1")
+            # .replace("threadIdx.z", "1")
         )
+
+        # print(codeText)
+
         self = MeasuredMetrics()
-        (
-            self.memLoad,
-            self.memStore,
-            # self.L2Load_tex,
-            # self.L2Store,
-            # self.L1Wavefronts,
-        ) = stencil_runner.measureMetrics2BufferKernel(
-            lc.API,
-            ["dram_load_bytes", "dram_write_bytes"],
-            codeText,
-            "kernel",
-            lc.block,
-            lc.grid,
-            lc.bufferSizeBytes,
-        )
 
-        self.memLoad *= 1024.0 / (lc.domain[0] * lc.domain[1] * lc.domain[2])
-        self.memStore *= 1024.0 / (lc.domain[0] * lc.domain[1] * lc.domain[2])
+        if lc.API == "HIP":
+            (
+                self.memLoad,
+                self.memStore,
+            ) = stencil_runner.measureMetrics2BufferKernel(
+                lc.API,
+                ["FETCH_SIZE", "WRITE_SIZE"],
+                codeText,
+                "kernel",
+                lc.block,
+                lc.grid,
+                lc.bufferSizeBytes,
+                lc.alignmentBytes,
+            )
 
-        self.L2Load_tex = 222;
-        self.L2Store = 333;
-        self.L1Wavefronts = 444;
+            (
+                self.L2Load_tex,
+                self.L2Store,
+                self.L1Wavefronts_TD,
+                self.L1Wavefronts_TA,
+                self.UTCL1_requests,
+                self.UTCL1_miss,
+            ) = stencil_runner.measureMetrics2BufferKernel(
+                lc.API,
+                [
+                    "TCP_TCC_READ_REQ_sum",
+                    "TCP_TCC_WRITE_REQ_sum",
+                    "TD_TD_BUSY_sum",
+                    "TA_TA_BUSY_sum",
+                    "TCP_UTCL1_REQUEST_sum",
+                    "TCP_UTCL1_TRANSLATION_MISS_sum",
+                ],
+                codeText,
+                "kernel",
+                lc.block,
+                lc.grid,
+                lc.bufferSizeBytes,
+                lc.alignmentBytes,
+            )
+
+            self.memLoad *= 1024.0 / lc.lupCount
+            self.memStore *= 1024.0 / lc.lupCount
+
+            self.L2Load_tex *= 64.0 / lc.lupCount
+            self.L2Load = self.L2Load_tex
+            self.L2Store *= 64.0 / lc.lupCount
+
+            self.L1Wavefronts_TA *= 1.0 / lc.lupCount
+            self.L1Wavefronts_TD *= 1.0 / lc.lupCount
+            self.UTCL1_requests *= 1.0 / lc.lupCount
+            self.UTCL1_miss *= 1.0 / lc.lupCount
+            self.L1Wavefronts = self.L1Wavefronts_TA
+        else:
+            (
+                self.memLoad,
+                self.memStore,
+                self.L2Load,
+                self.L2Store,
+                self.L2Load_tex,
+                self.L2Store_tex,
+                self.L2tex,
+                # self.L2ltc,
+                # self.L2total,
+                self.L2tagRequests,
+                self.L1Wavefronts,
+            ) = stencil_runner.measureMetrics2BufferKernel(
+                lc.API,
+                [
+                    "dram__bytes_read.sum",
+                    "dram__bytes_write.sum",
+                    "lts__t_sectors_op_read.sum",
+                    "lts__t_sectors_op_write.sum",
+                    "lts__t_sectors_srcunit_tex_op_read.sum",
+                    "lts__t_sectors_srcunit_tex_op_write.sum",
+                    "lts__t_sectors_srcunit_tex.sum",
+                    "lts__t_tag_requests.sum",
+                    "l1tex__data_pipe_lsu_wavefronts.sum",
+                ],
+                codeText,
+                "kernel",
+                lc.block,
+                lc.grid,
+                lc.bufferSizeBytes,
+                lc.alignmentBytes,
+            )
+            self.memLoad *= 1 / lc.lupCount
+            self.memStore *= 1 / lc.lupCount
+            self.L2Store *= 32 / lc.lupCount
+            self.L2Load *= 32 / lc.lupCount
+            self.L2Store_tex *= 32 / lc.lupCount
+            self.L2Load_tex *= 32 / lc.lupCount
+            self.L2Load = self.L2Load_tex
+            self.L2Store = self.L2Store_tex
+            self.L2Load *= 32 / lc.lupCount
+            self.L2tex *= 32 / lc.lupCount
+            self.L2tagRequests *= 1 / lc.lupCount
+            self.L1Wavefronts *= 1 / lc.lupCount
 
         self.L2ltc = 1
         self.L2total = 1
 
         time = stencil_runner.time2BufferKernel(
-            lc.API, codeText, "kernel", lc.block, lc.grid, lc.bufferSizeBytes
+            lc.API,
+            codeText,
+            "kernel",
+            lc.block,
+            lc.grid,
+            lc.bufferSizeBytes,
+            lc.alignmentBytes,
         )
         self.lups = lc.domain[0] * lc.domain[1] * lc.domain[2] / time / 1e9
 
