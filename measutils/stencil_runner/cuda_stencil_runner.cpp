cdf0cdfebbb8b9d00f9f7d27670378a41bfcb7ea
Dominik Ernst <dominik.ernst@fau.de>
2023-06-30 11:57:56 +0200

    measutils: move cuda rtc in separate file, add nbufferkernels

--- measutils/stencil_runner/cuda_stencil_runner.cpp
+++ measutils/stencil_runner/cuda_stencil_runner.cpp
@@ -1,136 +1,8 @@
-#include "../gpu_metrics/gpu_metrics.hpp"
-#include "MeasurementSeries.hpp"
-#include "dtime.hpp"
+#include "cuda_rtc.hpp"
 #include <Python.h>
-#include <cuda.h>
-#include <cuda_runtime.h>
-#include <iomanip>
-#include <iostream>
-#include <map>
-#include <nvrtc.h>
-#include <string>
 #include <vector>
+
 using namespace std;
-
-#ifdef __HIP__
-#define checkCudaErrors(ans)                                                   \
-  { gpuAssert((ans), __FILE__, __LINE__); }
-inline void gpuAssert(hipError_t code, const char *file, int line,
-                      bool abort = true) {
-  if (code != hipSuccess) {
-    std::cerr << "GPUassert: \"" << hipGetErrorString(code) << "\"  in " << file
-              << ": " << line << "\n";
-    if (abort)
-      exit(code);
-  }
-}
-#else
-
-#define checkCudaErrors(err) __checkCudaErrors(err, __FILE__, __LINE__)
-inline void __checkCudaErrors(CUresult err, const char *file, const int line) {
-  if (CUDA_SUCCESS != err) {
-    const char *errorStr = NULL;
-    cuGetErrorString(err, &errorStr);
-    fprintf(stderr,
-            "checkCudaErrors() Driver API error = %04d \"%s\" from file <%s>, "
-            "line %i.\n",
-            err, errorStr, file, line);
-  }
-}
-
-#endif
-
-#define NVRTC_SAFE_CALL(x)                                                     \
-  do {                                                                         \
-    nvrtcResult result = x;                                                    \
-    if (result != NVRTC_SUCCESS) {                                             \
-      std::cerr << "\nerror: " #x " failed with error "                        \
-                << nvrtcGetErrorString(result) << '\n';                        \
-    }                                                                          \
-  } while (0)
-
-CUdeviceptr src = 0;
-CUdeviceptr dst = 0;
-size_t bufferSizeBytes = 0;
-CUcontext context;
-bool initialized = false;
-
-void init(size_t newBufferSizeBytes) {
-  bufferSizeBytes += 1024;
-  setenv("HSA_TOOLS_LIB", "/opt/rocm/rocprofiler/lib/librocprofiler64.so", 1);
-  setenv("ROCP_METRICS", "/opt/rocm/lib/rocprofiler/metrics.xml", 1);
-  setenv("ROCP_HSA_INTERCEPT", "1", 1);
-  if (!initialized) {
-    checkCudaErrors(cuInit(0));
-    CUdevice cuDevice;
-    checkCudaErrors(cuDeviceGet(&cuDevice, 0));
-    checkCudaErrors(cuCtxCreate(&context, 0, cuDevice));
-    initialized = true;
-  }
-  if (bufferSizeBytes < newBufferSizeBytes) {
-
-    newBufferSizeBytes = std::max(
-        (size_t)1024 * 1024,
-        std::max(newBufferSizeBytes, std::min((size_t)1024 * 1024 * 1024 * 10,
-                                              (size_t)newBufferSizeBytes * 2)));
-
-    std::cout << "allocate " << newBufferSizeBytes << ", was "
-              << bufferSizeBytes << "\n";
-    if (src != 0) {
-      checkCudaErrors(cuMemFree(src));
-      checkCudaErrors(cuMemFree(dst));
-    }
-    checkCudaErrors(cuMemAlloc(&src, newBufferSizeBytes));
-    checkCudaErrors(cuMemAlloc(&dst, newBufferSizeBytes));
-    bufferSizeBytes = newBufferSizeBytes;
-  }
-}
-
-std::map<std::string, CUfunction> funcCache;
-
-CUfunction buildKernel(const char *kernelText, const char *funcName) {
-
-  if (auto it = funcCache.find(std::string(kernelText));
-      it == funcCache.end()) {
-    nvrtcProgram prog;
-    NVRTC_SAFE_CALL(nvrtcCreateProgram(&prog, kernelText, NULL, 0, NULL, NULL));
-
-#ifdef __NVCC__
-    const char *opts[] = {"-arch=sm_80"};
-#else
-    const char *opts[] = {};
-#endif
-    NVRTC_SAFE_CALL(
-        nvrtcCompileProgram(prog, sizeof(opts) / sizeof(char *), opts));
-
-    size_t logSize;
-    NVRTC_SAFE_CALL(nvrtcGetProgramLogSize(prog, &logSize));
-    char *log = new char[logSize];
-    NVRTC_SAFE_CALL(nvrtcGetProgramLog(prog, log));
-
-    if (logSize > 1) {
-      std::cout << log << "\n";
-      std::cout << kernelText << "\n";
-      std::cout << funcName << "\n";
-    }
-    size_t ptxSize;
-    NVRTC_SAFE_CALL(nvrtcGetPTXSize(prog, &ptxSize));
-    char *ptx = new char[ptxSize];
-    NVRTC_SAFE_CALL(nvrtcGetPTX(prog, ptx));
-
-    // std::cout << ptx << "\n";
-
-    CUmodule module;
-    CUfunction kernel;
-    checkCudaErrors(cuModuleLoadDataEx(&module, ptx, 0, 0, 0));
-    checkCudaErrors(cuModuleGetFunction(&kernel, module, funcName));
-
-    funcCache.insert({std::string(kernelText), kernel});
-    // NVRTC_SAFE_CALL(nvrtcDestroyProgram(&prog));
-  }
-
-  return funcCache[kernelText];
-}
 
 extern "C" double timeKernel(const char *kernelText, const char *funcName,
                              int blockSizeX, int blockSizeY, int blockSizeZ,
@@ -142,8 +14,8 @@ extern "C" double timeKernel(const char *kernelText, const char *funcName,
   auto kernel = buildKernel(kernelText, funcName);
 
   std::cout << alignmentBytes << " alignment Bytes\n";
-  void* alignedSrc = (char *)src + 1024 - alignmentBytes;
-  void* alignedDst = (char *)dst + 1024 - alignmentBytes;
+  void *alignedSrc = (char *)src + 1024 - alignmentBytes;
+  void *alignedDst = (char *)dst + 1024 - alignmentBytes;
   void *args[] = {&alignedSrc, &alignedDst};
 
   MeasurementSeries times;
@@ -163,6 +35,65 @@ extern "C" double timeKernel(const char *kernelText, const char *funcName,
   return times.median();
 }
 
+#ifndef NO_PYTHON
+extern "C" double pyTimeNBufferKernel(PyObject *kernelText, PyObject *funcName,
+                                      PyObject *blockSize, PyObject *blockCount,
+                                      PyObject *buffers,
+                                      PyObject *alignmentBytes) {
+
+  size_t totalBufferSizeBytes = 0;
+  for (int i = 0; i < PyList_Size(buffers); i++) {
+    totalBufferSizeBytes += PyLong_AsLong((PyList_GetItem(buffers, i))) + 1024;
+  }
+
+  init(totalBufferSizeBytes);
+  void **bufferPointers =
+      (void **)malloc(PyList_Size(buffers) * sizeof(void *));
+  void **bufferPointersPointers =
+      (void **)malloc(PyList_Size(buffers) * sizeof(void *));
+
+  void *currPtr = (char *)src;
+
+  for (int i = 0; i < PyList_Size(buffers); i++) {
+    bufferPointers[i] = (char *)currPtr + 1024 - PyLong_AsLong(alignmentBytes);
+    currPtr =
+        (char *)currPtr + PyLong_AsLong((PyList_GetItem(buffers, i))) + 1024;
+    bufferPointersPointers[i] = &bufferPointers[i];
+  }
+
+  auto kernel = buildKernel((char *)PyUnicode_1BYTE_DATA(kernelText),
+                            (char *)PyUnicode_1BYTE_DATA(funcName));
+
+  MeasurementSeries times;
+
+  cudaDeviceSynchronize();
+  for (int i = 0; i < 11; i++) {
+    cudaDeviceSynchronize();
+    double t1 = dtime();
+    checkCudaErrors(
+        cuLaunchKernel(kernel, PyLong_AsLong(PyTuple_GetItem(blockCount, 0)),
+                       PyLong_AsLong(PyTuple_GetItem(blockCount, 1)),
+                       PyLong_AsLong(PyTuple_GetItem(blockCount, 2)),
+                       PyLong_AsLong(PyTuple_GetItem(blockSize, 0)),
+                       PyLong_AsLong(PyTuple_GetItem(blockSize, 1)),
+                       PyLong_AsLong(PyTuple_GetItem(blockSize, 2)), 0, NULL,
+                       (void **)bufferPointersPointers, 0));
+
+    /*checkCudaErrors(cuLaunchKernel(kernel, 1, 1, 1, 1, 1, 1, 0, 0,
+                                   (void **)bufferPointersPointers, nullptr));*/
+    cudaDeviceSynchronize();
+    double t2 = dtime();
+    times.add(t2 - t1);
+  }
+
+  free(bufferPointers);
+  free(bufferPointersPointers);
+
+  return times.median();
+  return 1.0f;
+}
+#endif
+
 vector<double> measureMetrics(vector<const char *> metricNames,
                               const char *kernelText, const char *funcName,
                               int blockSizeX, int blockSizeY, int blockSizeZ,
@@ -170,12 +101,11 @@ vector<double> measureMetrics(vector<const char *> metricNames,
                               size_t bufferSizeBytes, size_t alignmentBytes) {
   init(bufferSizeBytes);
 
-
   auto kernel = buildKernel(kernelText, funcName);
 
   std::cout << alignmentBytes << " alignment Bytes\n";
-  void* alignedSrc = (char *)src + 1024 - alignmentBytes;
-  void* alignedDst = (char *)dst + 1024 - alignmentBytes;
+  void *alignedSrc = (char *)src + 1024 - alignmentBytes;
+  void *alignedDst = (char *)dst + 1024 - alignmentBytes;
   void *args[] = {&alignedSrc, &alignedDst};
   measureMetricsStart(metricNames);
 
@@ -231,9 +161,9 @@ int main(int argc, char **) {
   setenv("ROCP_HSA_INTERCEPT", "1", 1);
 
   for (size_t i = 1; i < (size_t)8 * 1024 * 1024 * 1024; i *= 2) {
-    string codeString =
-        "extern \"C\" __global__ void updateKernel(double* A, double* B) {int "
-        "tidx = threadIdx.x + blockDim.x * blockIdx.x;";
+    string codeString = "extern \"C\" __global__ void updateKernel(double* "
+                        "A, double* B) {int "
+                        "tidx = threadIdx.x + blockDim.x * blockIdx.x;";
     codeString += "size_t elementCount = " + to_string(i) + ";\n";
     codeString += "for(size_t i = tidx; i < elementCount; i += blockDim.x * "
                   "gridDim.x) {A[i] = 0.2 * A[i];}}";
