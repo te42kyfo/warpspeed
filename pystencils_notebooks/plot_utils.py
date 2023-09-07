3328c40e4b45e2bd3b6a7b060f9a0cf9a9465691
Dominik Ernst <dominik.ernst@fau.de>
2023-09-07 17:06:41 +0200

    update plot_utils.py

--- pystencils_notebooks/plot_utils.py
+++ pystencils_notebooks/plot_utils.py
@@ -3,127 +3,105 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+import scipy.stats as stats
 import math
 import random
 
+markers = ["o", "s", "D", "P", "X", "h", "*", "v", "^", "<", ">"]
 
-def volumeScatterPlot(measurements, predictions, title=None, lims=None):
+
+def computeMape(values):
+    mape = 0
+    for v in values:
+        mape += abs(v[1] - v[2]) / v[1]
+    return mape / len(values)
+
+
+def getColor(b):
+    return tuple(
+        min(
+            1.0,
+            math.log2(c) / math.log2(128) * 1.5 / math.log2(b[0] * b[1] * b[2]) * 10,
+        )
+        for c in b
+    )
+
+
+def volumeScatterPlot(values, title=None, lims=None, linear=False):
     fig, ax = plt.subplots()
     fig.set_figwidth(4)
     fig.set_figheight(4)
     fig.set_dpi(150)
-    markers = [
-        #".",
-        "D",
-        "s",
-        "s",
-        "o",
-        "v",
-        "s",
-        "1",
-        "2",
-        "3",
-        "4",
-        "v",
-        "^",
-        "<",
-        ">",
-    ]
 
-    labels = ["", "DRAM", "L2", "L1", ""]
+    if len(values[0]) < 4:
+        values = [(*v, 0) for v in values]
 
-    for r in range(-1, 13):
-        keys = [
-            bc[1:]
-            for bc in measurements.keys()
-            if bc[0] == r
-            if predictions[bc] > 0.1 and measurements[bc] > 0.1
-        ]
-        if len(keys) == 0:
-            continue
-        random.shuffle(keys)
+    categories = sorted(list(set([v[3] for v in values])))
 
-        measuredValues = [measurements[(r, *key)] for key in keys]
-        predictedValues = [predictions[(r, *key)] for key in keys]
+    for catID, catName in enumerate(categories):
+        catValues = [v for v in values if v[3] == catName]
+        random.shuffle(catValues)
 
-        colors = [tuple(math.log2(bc) for bc in b) for b in keys]
+        measValues = [v[1] for v in catValues]
+        predValues = [v[2] for v in catValues]
 
-        maxColors = [max([color[i] for color in colors]) for i in range(len(colors[0]))]
-        maxSize = max([sum(color) for color in colors])
-        colors = [
-            tuple(
-                [
-                    min(1.0, color[i] / math.log2(128) * 1.5 )
-                    for i in range(len(colors[0]))
-                ]
-                + [0] * (3 - len(colors[0]))
-            )
-            for color in colors
-        ]
-
-        colorArray = [(1.0, 0.0, 0.0), (1.0, 0.9, 0.0), (0.0, 1.0, 0.0), (0.0, 0.9, 0.7), (0.0, 0.0, 1.0), (0.7, 0.0, 0.7)]
-
-        #colors = len(colors) * [colorArray[r-1]]
+        colors = [getColor(v[0]) for v in catValues]
         ax.scatter(
-            measuredValues,
-            predictedValues,
+            measValues,
+            predValues,
             s=[600] * len(colors),
             marker="o",
             c=colors,
-            alpha=0.05,
+            alpha=0.03,
             edgecolors="none",
         )
         ax.scatter(
-            measuredValues,
-            predictedValues,
-            s=[40] * len(colors),
+            measValues,
+            predValues,
+            s=[50] * len(colors),
             c=colors,
-            marker=markers[r + 1],
+            marker=markers[catID],
             alpha=1,
             edgecolors="none",
-            label=labels[r],
+            label=catName,
         )
 
-        plot_range = (min(ax.get_xlim()[0], ax.get_ylim()[0]),
-                      max(ax.get_xlim()[1], ax.get_ylim()[1]))
-        for key, m, p, color in zip(keys, measuredValues, predictedValues, colors):
-            break
-            rect_size = [ math.sqrt(k) * (math.log(plot_range[1]) - math.log(plot_range[0]))
-                for k in key[0:3]
-            ]
-            rect_size[0] = 0.004 * rect_size[0] * m
-            rect_size[1] = 0.004 * rect_size[1] * p
-            rect_size.append( 0.004*rect_size[2] * p)
-            rect_size[2] = 0.004 * rect_size[2] * m
+        if len(catValues[0]) > 4:
+            selectedValues = [v for v in catValues if abs(v[2] - v[4]) / v[2] > 0.1]
+            selectedMeas = [v[1] for v in selectedValues]
+            selectedPred = [v[4] for v in selectedValues]
 
-
-            rect = patches.Rectangle(
-                (m - rect_size[0], p - rect_size[1]),
-
-                rect_size[0]*2.0,
-                rect_size[1]*2.0,
-                linewidth=1,
-                edgecolor="black",
-                facecolor="None",
-                alpha =0.2,
+            ax.scatter(
+                selectedMeas,
+                selectedPred,
+                s=5,
+                color="none",
+                marker=markers[catID],
+                alpha=0.5,
+                edgecolors="black",
+                zorder=-1,
             )
-            ax.add_patch(rect)
-            ax.plot( (m-rect_size[0],m+rect_size[0]), (p,p) , color=color, linewidth=1, alpha=0.3  )
-            ax.plot( (m,m), (p - rect_size[1],p+rect_size[1]) , color=color , linewidth=1.0, alpha=0.3 )
-            ax.plot( (m-rect_size[2],m+rect_size[2]), (p-rect_size[3],p+rect_size[3])  , color=color, linewidth=1.0, alpha=0.3 )
-
-        #ax.scatter(
-        #    measuredValues,
-        #    predictedValues,
-        #    s=[50] * len(colors),
-        #    c=colors,
-        #    marker=markers[r + 1],
-        #    alpha=1,
-        #    edgecolors="none",
-        #    label=str(r),
-        #)
-    ax.set_xscale("log", base=1.5)
-    ax.set_yscale("log", base=1.5)
+            for v in selectedValues:
+                ax.arrow(
+                    v[1],
+                    v[4],
+                    0,
+                    v[2] - v[4],
+                    linewidth=0.5,
+                    alpha=0.5,
+                    head_width=0.05 * v[1],
+                    fill=False,
+                    head_length=0.05 * v[4],
+                    length_includes_head=True,
+                    color="gray",
+                )
+    plot_range = (
+        min(ax.get_xlim()[0], ax.get_ylim()[0]),
+        max(ax.get_xlim()[1], ax.get_ylim()[1]),
+    )
+    if not linear:
+        ax.set_xscale("log", base=2)
+        ax.set_yscale("log", base=2)
 
     xlim = ax.get_xlim()
     ylim = ax.get_ylim()
@@ -131,16 +109,15 @@ def volumeScatterPlot(measurements, predictions, title=None, lims=None):
     ax.plot([0.1, 1150], [0.1, 1150], color="black", alpha=0.2)
 
     ax.set_xticks(
-        [0.1, 1.0, 2, 4, 8, 9, 12, 16, 20, 24, 32, 48, 64, 96, 128, 192, 256, 384, 512]
+        [0.1, 1.0, 2, 4, 8, 9, 12, 16, 24, 32, 48, 64, 96, 128, 192, 256, 384, 512]
     )
     ax.set_yticks(
-        [0.1, 1.0, 2, 4, 8, 9, 12, 16, 20, 24, 32, 48, 64, 96, 128, 192, 256, 384, 512]
+        [0.1, 1.0, 2, 4, 8, 9, 12, 16, 24, 32, 48, 64, 96, 128, 192, 256, 384, 512]
     )
 
     formatter = matplotlib.ticker.FuncFormatter(lambda x, pos: "{0:g}".format(x))
     ax.get_xaxis().set_major_formatter(formatter)
     ax.get_yaxis().set_major_formatter(formatter)
-
 
     if not lims is None:
         ax.set_xlim(lims)
@@ -148,10 +125,16 @@ def volumeScatterPlot(measurements, predictions, title=None, lims=None):
     else:
         lim = (min(ylim[0], xlim[0]), max(ylim[1], xlim[1]))
         ax.set_xlim(
-            (max(min(lim[0] * 0.9, lim[0] - 0.1), 0.2), max(1, lim[1] * 1.1, lim[1] + 0.1))
+            (
+                max(min(lim[0] * 0.9, lim[0] - 0.1), 0.2),
+                max(1, lim[1] * 1.1, lim[1] + 0.1),
+            )
         )
         ax.set_ylim(
-            (max(min(lim[0] * 0.9, lim[0] - 0.1), 0.2), max(1, lim[1] * 1.1, lim[1] + 0.1))
+            (
+                max(min(lim[0] * 0.9, lim[0] - 0.1), 0.2),
+                max(1, lim[1] * 1.1, lim[1] + 0.1),
+            )
         )
 
     if not title is None:
@@ -159,14 +142,31 @@ def volumeScatterPlot(measurements, predictions, title=None, lims=None):
 
     ax.set_xlabel("actual Volume, B/Lup")
     ax.set_ylabel("predicted Volume, B/Lup")
-    ax.legend()
+
+    tau, p_value = stats.kendalltau([v[1] for v in values], [v[2] for v in values])
+
+    # text = "MAPE: {:.1f}%,  Kendall's Tau: {:.2f}".format(computeMape(values)*100, tau)
+    text = "MAPE: {:.1f}%".format(computeMape(values) * 100)
+    ax.annotate(
+        text,
+        (0.04, 0.95),
+        xycoords="axes fraction",
+        va="center",
+        ha="left",
+        fontfamily="monospace",
+        fontsize="small",
+        bbox=dict(boxstyle="round", fc="#FFFFFFAA", ec="#CCCCCC00"),
+    )
+
+    ax.legend(loc="lower right")
     fig.tight_layout()
     ax.grid()
 
-    plt.savefig( "./autoplots/" + title + ".svg")
-    plt.show()
+    plt.savefig("./autoplots/" + title + ".svg")
+    # plt.show()
+    # plt.close()
 
-
+    return (fig, ax)
 
 
 def performanceScatterPlot(measuredValues, predictedValues):
@@ -175,10 +175,7 @@ def performanceScatterPlot(measuredValues, predictedValues):
     fig.set_figheight(4)
     fig.set_dpi(150)
 
-    colors = [
-        tuple(math.log2(bc) / math.log2(1024) for bc in b)
-        for b in measuredValues.keys()
-    ]
+    colors = [tuple(getColor(bc) for bc in b) for b in measuredValues.keys()]
 
     ax.scatter(
         measuredValues.values(),
@@ -196,7 +193,6 @@ def performanceScatterPlot(measuredValues, predictedValues):
         alpha=1,
         edgecolors="none",
     )
-    # ax.scatter(list(measuredValues.values()), list(predictedValues.values()), ".", markersize=1)
 
     fig.tight_layout()
 
@@ -221,5 +217,97 @@ def performanceScatterPlot(measuredValues, predictedValues):
     ax.set_ylabel("predicted Performance, GLup/s")
 
     ax.grid()
-    plt.savefig( "./autoplots/" + title + ".svg")
+    plt.tight_layout()
+    plt.savefig("./autoplots/" + title + ".svg")
+    # plt.show()
+    return (fig, ax)
+
+
+def scatterPlot(values, title=None, lims=None):
+    fig, ax = plt.subplots()
+    fig.set_figwidth(4)
+    fig.set_figheight(4)
+    fig.set_dpi(150)
+
+    if len(values[0]) < 4:
+        values = [(*v, 0) for v in values]
+
+    categories = sorted(list(set([v[3] for v in values])))
+
+    for catID, catName in enumerate(categories):
+        catValues = [v for v in values if v[3] == catName]
+        random.shuffle(catValues)
+
+        measValues = [v[1] for v in catValues]
+        predValues = [v[2] for v in catValues]
+
+        colors = [getColor(v[0]) for v in catValues]
+        ax.scatter(
+            measValues,
+            predValues,
+            s=[400] * len(colors),
+            marker="o",
+            c=colors,
+            alpha=0.05,
+            edgecolors="none",
+        )
+        ax.scatter(
+            measValues,
+            predValues,
+            s=[20] * len(colors),
+            c=colors,
+            marker=markers[catID],
+            alpha=1,
+            edgecolors="none",
+            label=catName,
+        )
+
+        if len(catValues[0]) > 4:
+            selectedValues = [v for v in catValues if abs(v[2] - v[4]) / v[2] > 0.05]
+            selectedMeas = [v[1] for v in selectedValues]
+            selectedPred = [v[4] for v in selectedValues]
+
+            ax.scatter(
+                selectedMeas,
+                selectedPred,
+                s=30,
+                color="none",
+                marker=markers[catID],
+                alpha=1,
+                edgecolors="gray",
+            )
+            for v in selectedValues:
+                ax.arrow(
+                    v[1],
+                    v[4],
+                    0,
+                    v[2] - v[4],
+                    linewidth=0.5,
+                    head_width=0.05 * v[1],
+                    fill=False,
+                    head_length=0.05 * v[4],
+                    length_includes_head=True,
+                    color="gray",
+                )
+    plot_range = (
+        min(ax.get_xlim()[0], ax.get_ylim()[0]),
+        max(ax.get_xlim()[1], ax.get_ylim()[1]),
+    )
+
+    if not lims is None:
+        ax.set_xlim(lims)
+        ax.set_ylim(lims)
+
+    if not title is None:
+        ax.set_title(title)
+
+    ax.set_xlabel("Quantity A")
+    ax.set_ylabel("Quantity B")
+    ax.legend()
+    fig.tight_layout()
+    ax.grid()
+
+    plt.savefig("./autoplots/" + title + ".svg")
     plt.show()
+    plt.close()
+    return (fig, ax)
